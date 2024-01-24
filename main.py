b4d495565658f7f0959e6253c18007b8f211d20f
Schizophrenia gaming (.py) <109104295+doggaming69@users.noreply.github.com>
2024-01-24 14:53:46 +0100

    Create main.py
    sorry for no commits for a few days

---
+++ main.py
@@ -0,0 +1,62 @@
+import random #DEEPWOKEN BUILD RANDOMIZER 
+
+def weapon():
+    Weapon = ["light", "medium", "heavy",]
+    weapon_choose = random.choice(Weapon)
+    return weapon_choose
+
+def attunement():
+    Attunement = ["flamecharm", "shadowcast", "frostdraw", "ironsing", "thundercall", "galebreath", "attunementless"]
+    attunement_choose = random.choice(Attunement)
+    return attunement_choose
+
+def oath():
+    Oath = ["oathless", "jetstriker", "blindseer", "dawnwalker", "fadetrimmer", "contractor", "linkstrider", "visionshaper", "starkindred"]
+    Oath_choose = random.choice(Oath)
+    return Oath_choose
+
+def archetype():
+    Archetype = ['tank', 'damage dealer', 'support', 'mage', 'anti healer']
+    Archetype_choose = random.choice(Archetype)
+    return Archetype_choose
+
+def bell_usage():
+    Bell_usage = ["gank bell", "support bell", "1v1 bell", "misc (teleport kamui etc)"]
+    Bell_usage_choose = random.choice(Bell_usage)
+    return Bell_usage_choose
+
+def medium_weapon():
+    Medium_weapon = ['sword','spear','rifle']
+    Medium_weapon_choose = random.choice(Medium_weapon)
+    return Medium_weapon_choose
+
+def light_weapon():
+    Light_weapon = ['dagger','fist','rapier','guns','jus karita']
+    Light_weapon_choose = random.choice(Light_weapon)
+    return Light_weapon_choose
+
+def heavy_weapon():
+    Heavy_weapon = ['greataxe','halberd','greathammer','misc (for example petra anchor)']
+    Heavy_weapon_choose = random.choice(Heavy_weapon)
+    return Heavy_weapon_choose
+
+while True:
+    deepwoken = input("Press any key to continue, or Enter to exit: ")
+    if deepwoken != "":
+        break
+
+print("Weapon:", weapon())
+print("Attunement:", attunement())
+print("Oath:", oath())
+print("Archetype:", archetype())
+print("Bell Usage:", bell_usage())
+while True:
+    deepwoken2 = input ('enter weapon type that you recieved (type no if you want the freedom to choose it urself)')
+    if deepwoken2 !="":
+        break
+if deepwoken2 == ('medium'):
+    print ("Weapon variant:", medium_weapon())
+if deepwoken2 == ('heavy'):
+    print ("Weapon variant:", heavy_weapon())
+if deepwoken2 == ('light'):
+    print ("Weapon variant:",light_weapon())
